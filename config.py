f06b537191f20a1c0eac154dcdae9a5e085ba7cc
Anozie Joel <chibuikeanozie0@gmail.com>
2023-06-23 22:05:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,3 @@
-import os
-
-bearer_token=os.environ["bearer_token"]
-consumer_key=os.environ["consumer_key"]
-consumer_secret=os.environ["consumer_secret"]
-access_token=os.environ["access_token"]
-access_token_secret=os.environ["access_token_secret"]
-
-'''
-Uncomment the below code and run script to confirm keys are set
-dont forget to comment the below code back when check is done
-'''
-# print(f"bearer_token : {bearer_token}\n\nconsumer_key : {consumer_key} \n\nconsumer_secret : {consumer_secret} \n\naccess_token : {access_token} \n\naccess_token_secret : {access_token_secret}")
-
-
-
-
-
-
 '''
 Set your configuration key by running the below code in your shell/terminal : 
 export bearer_token="your bearer token"
@@ -26,4 +7,16 @@ export access_token="your access token"
 export access_token_secret="your access token secret"
 '''
 
+import os
+
+bearer_token=os.environ["bearer_token"]
+consumer_key=os.environ["consumer_key"]
+consumer_secret=os.environ["consumer_secret"]
+access_token=os.environ["access_token"]
+access_token_secret=os.environ["access_token_secret"]
+
+print(f"bearer_token : {bearer_token}\n\nconsumer_key : {consumer_key} \n\nconsumer_secret : {consumer_secret} \n\naccess_token : {access_token} \n\naccess_token_secret : {access_token_secret}")
+
+if __name__ == '__main__':
+    main()
 
